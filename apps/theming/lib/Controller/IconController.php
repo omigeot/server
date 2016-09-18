19543a2d8dd291c8958645b807317f38c2e5571a
Julius Haertl <jus@bitgrid.net>
2016-09-18 20:15:06 +0200

    Caching for icon files using AppData
    Signed-off-by: Julius Haertl <jus@bitgrid.net>

--- apps/theming/lib/Controller/IconController.php
+++ apps/theming/lib/Controller/IconController.php
@@ -27,9 +27,15 @@ use OCA\Theming\ThemingDefaults;
 use OCP\AppFramework\Controller;
 use OCP\AppFramework\Http;
 use OCP\AppFramework\Http\DataDisplayResponse;
+use OCP\AppFramework\Http\FileDisplayResponse;
 use OCP\AppFramework\Utility\ITimeFactory;
+use OCP\Files\IAppData;
+use OCP\Files\SimpleFS\ISimpleFolder;
+use OCP\Files\SimpleFS\ISimpleFile;
 use OCP\IRequest;
 use OCA\Theming\Util;
+use OCP\IConfig;
+use OCP\Files\NotFoundException;
 
 class IconController extends Controller {
 	/** @var ThemingDefaults */
@@ -38,8 +44,12 @@ class IconController extends Controller {
 	private $util;
 	/** @var ITimeFactory */
 	private $timeFactory;
+	/** @var IConfig */
+	private $config;
 	/** @var IconBuilder */
 	private $iconBuilder;
+	/** @var IAppData */
+	private $appData;
 
 	/**
 	 * IconController constructor.
@@ -49,7 +59,9 @@ class IconController extends Controller {
 	 * @param ThemingDefaults $themingDefaults
 	 * @param Util $util
 	 * @param ITimeFactory $timeFactory
+	 * @param IConfig $config
 	 * @param IconBuilder $iconBuilder
+	 * @param IAppData $appData
 	 */
 	public function __construct(
 		$appName,
@@ -57,14 +69,18 @@ class IconController extends Controller {
 		ThemingDefaults $themingDefaults,
 		Util $util,
 		ITimeFactory $timeFactory,
-		IconBuilder $iconBuilder
+		IConfig $config,
+		IconBuilder $iconBuilder,
+		IAppData $appData
 	) {
 		parent::__construct($appName, $request);
 
 		$this->themingDefaults = $themingDefaults;
 		$this->util = $util;
 		$this->timeFactory = $timeFactory;
+		$this->config = $config;
 		$this->iconBuilder = $iconBuilder;
+		$this->appData = $appData;
 	}
 
 	/**
@@ -73,14 +89,18 @@ class IconController extends Controller {
 	 *
 	 * @param $app string app name
 	 * @param $image string image file name (svg required)
-	 * @return DataDisplayResponse
+	 * @return FileDisplayResponse
 	 */
 	public function getThemedIcon($app, $image) {
-		$image = $this->util->getAppImage($app, $image);
-		$svg = file_get_contents($image);
-		$color = $this->util->elementColor($this->themingDefaults->getMailHeaderColor());
-		$svg = $this->util->colorizeSvg($svg, $color);
-		$response = new DataDisplayResponse($svg, Http::STATUS_OK, ['Content-Type' => 'image/svg+xml']);
+		$iconFile = $this->getCachedImage("icon-" . $app . '-' . str_replace("/","_",$image));
+		if ($iconFile === null) {
+			$imageFile = $this->util->getAppImage($app, $image);
+			$svg = file_get_contents($imageFile);
+			$color = $this->util->elementColor($this->themingDefaults->getMailHeaderColor());
+			$svg = $this->util->colorizeSvg($svg, $color);
+			$iconFile = $this->setCachedImage("icon-" . $app . '-' . str_replace("/","_",$image), $svg);
+		}
+		$response = new FileDisplayResponse($iconFile, Http::STATUS_OK, ['Content-Type' => 'image/svg+xml']);
 		$response->cacheFor(86400);
 		$response->addHeader('Expires', date(\DateTime::RFC2822, $this->timeFactory->getTime()));
 		$response->addHeader('Pragma', 'cache');
@@ -94,12 +114,16 @@ class IconController extends Controller {
 	 * @NoCSRFRequired
 	 *
 	 * @param $app string app name
-	 * @return DataDisplayResponse
+	 * @return FileDisplayResponse|DataDisplayResponse
 	 */
-	public function getFavicon($app="core") {
-		if($this->themingDefaults->shouldReplaceIcons()) {
+	public function getFavicon($app = "core") {
+		$iconFile = $this->getCachedImage('favIcon-' . $app);
+		if($iconFile === null && $this->themingDefaults->shouldReplaceIcons()) {
 			$icon = $this->iconBuilder->getFavicon($app);
-			$response = new DataDisplayResponse($icon, Http::STATUS_OK, ['Content-Type' => 'image/x-icon']);
+			$iconFile = $this->setCachedImage('favIcon-' . $app, $icon);
+		}
+		if ($this->themingDefaults->shouldReplaceIcons()) {
+			$response = new FileDisplayResponse($iconFile, Http::STATUS_OK, ['Content-Type' => 'image/x-icon']);
 		} else {
 			$response = new DataDisplayResponse(null, Http::STATUS_NOT_FOUND);
 		}
@@ -116,12 +140,16 @@ class IconController extends Controller {
 	 * @NoCSRFRequired
 	 *
 	 * @param $app string app name
-	 * @return DataDisplayResponse
+	 * @return FileDisplayResponse|DataDisplayResponse
 	 */
-	public function getTouchIcon($app="core") {
-		if($this->themingDefaults->shouldReplaceIcons()) {
+	public function getTouchIcon($app = "core") {
+		$iconFile = $this->getCachedImage('touchIcon-' . $app);
+		if ($iconFile === null && $this->themingDefaults->shouldReplaceIcons()) {
 			$icon = $this->iconBuilder->getTouchIcon($app);
-			$response = new DataDisplayResponse($icon, Http::STATUS_OK, ['Content-Type' => 'image/png']);
+			$iconFile = $this->setCachedImage('touchIcon-' . $app, $icon);
+		}
+		if ($this->themingDefaults->shouldReplaceIcons()) {
+			$response = new FileDisplayResponse($$iconFile, Http::STATUS_OK, ['Content-Type' => 'image/png']);
 		} else {
 			$response = new DataDisplayResponse(null, Http::STATUS_NOT_FOUND);
 		}
@@ -129,6 +157,56 @@ class IconController extends Controller {
 		$response->addHeader('Expires', date(\DateTime::RFC2822, $this->timeFactory->getTime()));
 		$response->addHeader('Pragma', 'cache');
 		return $response;
+	}
+
+	/**
+	 * @param $filename
+	 * @return \OCP\Files\SimpleFS\ISimpleFile
+	 */
+	private function getCachedImage($filename) {
+		$currentFolder = $this->getCacheFolder();
+		if($currentFolder->fileExists($filename)) {
+			return $currentFolder->getFile($filename);
+		} else {
+			return null;
+		}
+	}
+
+	/**
+	 * @param $filename
+	 * @param $data
+	 * @return ISimpleFile
+	 */
+	private function setCachedImage($filename, $data) {
+		$currentFolder = $this->getCacheFolder();
+		$file = $currentFolder->fileExists($filename);
+		if ($file) {
+			$currentFolder->getFile($filename)->putContent($data);
+		} else {
+			$currentFolder->newFile($filename)->putContent($data);
+		}
+		return $currentFolder->getFile($filename);
+	}
+
+	/**
+	 * @return ISimpleFolder
+	 */
+	private function getCacheFolder() {
+
+		$cacheBusterValue = $this->config->getAppValue('theming', 'cachebuster', '0');
+		try {
+			$currentFolder = $this->appData->getFolder($cacheBusterValue);
+		} catch (NotFoundException $e) {
+			$currentFolder = $this->appData->newFolder($cacheBusterValue);
+			// cleanup old folders
+			$folders = $this->appData->getDirectoryListing();
+			foreach ($folders as $folder) {
+				if ($folder->getName() !== $currentFolder->getName()) {
+					$folder->delete();
+				}
+			}
+		}
+		return $currentFolder;
 	}
 
 }
